1c0648cd3fee258939ffbb0cc4dfe875dc32c605
ericz <1458341001@qq.com>
2019-09-09 16:44:06 +0800

    导航

--- OneGuyAPI/mainapp/admin.py
+++ OneGuyAPI/mainapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import OneGuoCity
+
+from .models import OneGuoCity, Navigationmodel, NavigationInfomodel
 
 
 class OneGuoCityAdmin(admin.ModelAdmin):
@@ -10,4 +11,14 @@ class OneGuoCityAdmin(admin.ModelAdmin):
     search_fields = ('city_name', 'py_name', 'is_hot')
 
 
+class NavigationmodelAdmin(admin.ModelAdmin):
+    list_display = ('Navigation_img1', 'Navigation_name', 'Navigation_id')
+
+
+class NavigationInfomodelAdmin(admin.ModelAdmin):
+    list_display = ('NavigationInfo_img1', 'NavigationInfo_name', 'NavigationInfo_id', 'user_id')
+
+
 admin.site.register(OneGuoCity, OneGuoCityAdmin)
+admin.site.register(Navigationmodel, NavigationmodelAdmin)
+admin.site.register(NavigationInfomodel, NavigationInfomodelAdmin)
--- OneGuyAPI/mainapp/models.py
+++ OneGuyAPI/mainapp/models.py
@@ -9,9 +9,29 @@ class OneGuoCity(models.Model):
     is_hot = models.CharField(max_length=10, verbose_name='是否热门', choices=hot, default=1)
     py_name = models.CharField(max_length=50, verbose_name='城市拼音名')
 
-    def __str__(self):
-        return self.city_name
 
-    class Meta:
-        db_table = 'OneGuoCity'
-        verbose_name_plural = verbose_name = '全国各大城市'
+class Navigationmodel(models.Model):
+    Navigation_img1 = models.ImageField(verbose_name='导航图片',
+                                        upload_to='goods')
+    Navigation_name = models.CharField(max_length=50,
+                                       verbose_name='导航名称')
+    Navigation_id = models.IntegerField(verbose_name='导航图片ID', )
+
+
+class NavigationInfomodel(models.Model):
+    NavigationInfo_img1 = models.ImageField(verbose_name='导航详情图片',
+                                            upload_to='goods')
+    NavigationInfo_name = models.CharField(max_length=50,
+                                           verbose_name='导航详情名称')
+    NavigationInfo_id = models.IntegerField(verbose_name='导航详情图片ID', )
+    user_id = models.CharField(max_length=30,
+                               verbose_name='用户ID')
+
+
+def __str__(self):
+    return self.city_name
+
+
+class Meta:
+    db_table = 'OneGuoCity'
+    verbose_name_plural = verbose_name = '全国各大城市'
