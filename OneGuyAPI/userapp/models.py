5d1118d09a8c96618b2935b6076094953c5dfa77
NAME <411176087@qq.com>
2019-09-09 13:48:52 +0800

    用户表

--- OneGuyAPI/userapp/models.py
+++ OneGuyAPI/userapp/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
 # Create your models here.
+from userapp.helper import make_password
+
+
+class OneGuoUser(models.Model):
+    level = (('0', '大众会员'),
+             ('1', '白银会员'),
+             ('2', '黄金会员'),
+             ('3', '钻石会员'))
+    sex = (('man', '男'),
+           ('woman', '女'))
+    phone = models.CharField(max_length=30,
+                             verbose_name='手机号',
+                             null=True)
+    name = models.CharField(max_length=20,
+                            verbose_name='昵称',
+                            null=True)
+    gender = models.CharField(max_length=10,
+                              verbose_name='性别',
+                              choices=sex,
+                              default='woman',
+                              null=True)    # 默认女 True 男 False
+    password = models.CharField(max_length=20,
+                                verbose_name='用户密码',
+                                null=True)
+    idcard = models.CharField(max_length=20,
+                              verbose_name='身份证',
+                              null=True)
+    img = models.ImageField(upload_to='img/%Y/%m/%d',
+                            verbose_name='用户头像',
+                            null=True,
+                            blank=True)
+    balance = models.FloatField(verbose_name='账户余额',
+                                default=0.0,
+                                null=True)
+    use_level = models.CharField(max_length=20,
+                                 choices=level,
+                                 verbose_name='用户等级',
+                                 default=0,
+                                 null=True)
+    is_active = models.BooleanField(verbose_name='是否激活',
+                                    default=False)
+    is_delete = models.BooleanField(verbose_name='是否删除',
+                                    default=False)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if len(self.password) < 6:
+            return ('密码必须由6-20位字符组成')
+        elif len(self.password) > 20:
+            return ('密码必须由6-20位字符组成')
+        else:
+            self.password = make_password(self.password)
+        super().save()
+
+
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = '用户表'
+        verbose_name_plural = verbose_name
