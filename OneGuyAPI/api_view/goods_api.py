9b09d0530cd60345b5323524b8f665caa5d1e02a
yifaniii <mhr180232@163.com>
2019-09-11 19:17:59 +0800

    增加orderapi

--- OneGuyAPI/api_view/goods_api.py
+++ OneGuyAPI/api_view/goods_api.py
@@ -5,32 +5,56 @@ from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
-from api.goods import Goods_cartModelSerializer, GoodsModelSerializer
+from api.goods import Goods_cartModelSerializer, GoodsModelSerializer, OrderSerializer, OrderDetailSerializer
 from goodsapp.models import Goods_cartModelEntity, GoodsModelEntity
+from orderapp.models import OrderModel, OrderDetailModel
 
 from django.views import View
 
+
 def goods_api():
     data = GoodsModelEntity.objects.all()
-    datas = GoodsModelSerializer(data,many=True)
+    datas = GoodsModelSerializer(data, many=True)
     content = JSONRenderer().render(datas.data)
     buffer = BytesIO(content)
     text = JSONParser().parse(buffer)
     return text
+
 
 def goods_cart_api():
     data = Goods_cartModelEntity.objects.all()
-    datas = Goods_cartModelSerializer(data,many=True)
+    datas = Goods_cartModelSerializer(data, many=True)
     content = JSONRenderer().render(datas.data)
     buffer = BytesIO(content)
     text = JSONParser().parse(buffer)
     return text
 
+
 class Goods_Api_View(View):
-    def get(self,request):
+    def get(self, request):
         data = GoodsModelEntity.objects.all()
-        serialize = GoodsModelSerializer(data,many=True)
+        serialize = GoodsModelSerializer(data, many=True)
         content = JSONRenderer().render(serialize.data)
         buffer = BytesIO(content)
         text = JSONParser().parse(buffer)
-        return JsonResponse(text,safe=False)
+        return JsonResponse(text, safe=False)
+
+
+class Order_Api_View(View):
+    def get(self, request):
+        data = OrderModel.objects.all()
+        serialize = OrderSerializer(data, many=True)
+        content = JSONRenderer().render(serialize.data)
+        buffer = BytesIO(content)
+        text = JSONParser().parse(buffer)
+        return JsonResponse(text, safe=False)
+
+
+class OrderDetail_Api_View(View):
+    def get(self, request):
+        data = OrderDetailModel.objects.all()
+        serialize = OrderDetailSerializer(data, many=True)
+        content = JSONRenderer().render(serialize.data)
+        buffer = BytesIO(content)
+        text = JSONParser().parse(buffer)
+        return JsonResponse(text, safe=False)
